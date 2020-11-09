ebeafbfa939559319df9a8bbbe452ce5f5497412
abdul-mohsen <abdulmohsen.jawad.almajed@gmail.com>
2020-11-09 11:39:08 +0300

    Added the project and the needed lib

---
+++ app/src/main/java/com/safcsp/foodstation/Client/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.safcsp.foodstation.Client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
