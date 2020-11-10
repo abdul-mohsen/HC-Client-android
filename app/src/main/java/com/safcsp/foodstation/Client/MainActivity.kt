ad2e8ef997fdcc45b658aeddd3373cc37b25fb64
abdul-mohsen <abdulmohsen.jawad.almajed@gmail.com>
2020-11-10 12:56:32 +0300

    basic nnavigation is complete

--- app/src/main/java/com/safcsp/foodstation/Client/MainActivity.kt
+++ app/src/main/java/com/safcsp/foodstation/Client/MainActivity.kt
@@ -2,10 +2,29 @@ package com.safcsp.foodstation.Client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val navControl = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navControl)
+
     }
 }
